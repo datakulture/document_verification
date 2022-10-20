9c874a126a2277e16ed8f48dec0156b68003d73f
DataKulture <115214447+datakulture@users.noreply.github.com>
2022-10-20 10:49:35 +0530

    logo added

--- aadhar_try.py
+++ aadhar_try.py
@@ -12,7 +12,7 @@ import re
 #import numpy as np
 from PIL import Image
 
-
+st.sidebar.image("dk_logo.png", use_column_width=False)
 #adding a file uploader
 st.title("Aadhar number extractor")
 st.write("Optical Character Recognition (OCR) implementation in Python")
